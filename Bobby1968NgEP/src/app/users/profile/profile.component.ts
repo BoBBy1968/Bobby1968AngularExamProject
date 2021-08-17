98fd43646d573da43a5bf683e13fb6b331b89ce8
BoBBy1968 <b.a.stanchev@gmail.com>
2021-08-17 12:08:31 +0300

    View profile works

--- Bobby1968NgEP/src/app/users/profile/profile.component.ts
+++ Bobby1968NgEP/src/app/users/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/interfaces';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  get user() {
+    return this.userService.user;
+  }
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
   }
